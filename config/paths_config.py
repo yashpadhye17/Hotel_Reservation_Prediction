26ca60d612c0d89810d5095a45ff6fca6bbd5a69
YASH PADHYE <yashpadhye2016@gmail.com>
2025-10-06 17:13:09 -0500

    Model Training and Versioning

--- config/paths_config.py
+++ config/paths_config.py
@@ -16,3 +16,6 @@ PROCESSED_TRAIN_DATA_PATH = os.path.join(PROCESSED_DIR,"processed_train.csv")
 PROCESSED_TEST_DATA_PATH = os.path.join(PROCESSED_DIR,"processed_test.csv")
 
 
+##########MODEL TRAINING#######
+
+MODEL_OUTPUT_PATH = "artifacts/models/lgbm_model.pkl"
