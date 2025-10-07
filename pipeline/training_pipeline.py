3fa0abb7446b558b0a83f85628823e1edaa07b61
YASH PADHYE <yashpadhye2016@gmail.com>
2025-10-06 20:19:29 -0500

    Minor Changes, Pipeline File created

---
+++ pipeline/training_pipeline.py
@@ -0,0 +1,19 @@
+from src.data_ingestion import DataIngestion
+from src.data_preprocessing import DataProcessor
+from src.model_training import ModelTraining
+from utils.common_functions import read_yaml
+from config.paths_config import *
+
+
+if __name__ == "__main__":
+    ##Data Ingestion
+    data_ingestion = DataIngestion(read_yaml(CONFIG_PATH))
+    data_ingestion.run()
+
+    ##Data Processing
+    processor = DataProcessor(TRAIN_FILE_PATH,TEST_FILE_PATH,PROCESSED_DIR,CONFIG_PATH)
+    processor.process()
+
+    ## Model Training
+    trainer = ModelTraining(PROCESSED_TRAIN_DATA_PATH,PROCESSED_TEST_DATA_PATH,MODEL_OUTPUT_PATH)
+    trainer.run()
